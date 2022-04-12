66b094bdfe41a5ec39e38e9cfe5463f517f8746d
404-error-404 <1791781644@qq.com>
2022-04-12 16:20:09 +0800

    feature: 完成左旋字符串

---
+++ src/leetcode/字符串/KMP模式匹配/So28实现strStr.java
@@ -0,0 +1,60 @@
+package leetcode.字符串.KMP模式匹配;
+
+import java.util.Locale;
+
+/**
+ * 实现 strStr() 函数。
+ * <p>
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+ * <p>
+ *
+ * <p>
+ * 说明：
+ * <p>
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * <p>
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-strstr
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author 小乐乐
+ * @date 2022/3/13 22:34
+ **/
+public class So28实现strStr {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0 || haystack.length() == 0) {
+            return -1;
+        }
+        needle.charAt(1);
+        char[] needleChars = needle.toCharArray();
+        // 构造next数组
+        int[] next = new int[needleChars.length];
+        int j = -1;
+        next[0] = j;
+        for (int i = 1; i < needleChars.length; i++) {
+            while (j >= 0 && needleChars[i] != needleChars[j + 1]) {
+                j = next[j];
+            }
+            if (needleChars[i] == needleChars[j + 1]) {
+                j++;
+            }
+            next[i] = j;
+        }
+        j = -1;
+        char[] s = haystack.toCharArray();
+        for (int i = 0; i < s.length; i++) {
+            while (j >= 0 && s[i] != next[j + 1]) {
+                j = next[j];
+            }
+            if (s[i] == next[j + 1]) {
+                j++;
+            }
+            if (j == s.length - 1) {
+                return i - next.length + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/leetcode/字符串/坐旋转字符串/Offer58Ⅱ左旋转字符串.java
@@ -0,0 +1,29 @@
+package leetcode.字符串.坐旋转字符串;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author 小乐乐
+ * @date 2022/3/13 19:40
+ **/
+public class Offer58Ⅱ左旋转字符串 {
+    public String reverseLeftWords(String s, int n) {
+        char[] chars = s.toCharArray();
+        return String.valueOf(Arrays.copyOfRange(chars, n, chars.length)) + String.valueOf(Arrays.copyOfRange(chars, 0, n));
+    }
+
+    @Test
+    public void test(){
+        String s = "阿萨开连接方式克拉的房间里";
+        int n = 2;
+        System.out.println(reverseLeftWords(s, n));
+    }
+}
