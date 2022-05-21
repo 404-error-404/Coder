f9e87961ac91ce3f0b6656968ee0f6af5e4d264c
404-error-404 <1791781644@qq.com>
2022-05-21 22:33:18 +0800

    feature: 78场双周赛，最后hard不会做

---
+++ src/leetcode/周赛/双周赛/2022/5/14/So2272最大波动的子字符串.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2022/5/21 21:04
+# @Remarks  :
+# 字符串的 波动 定义为子字符串中出现次数 最多 的字符次数与出现次数 最少 的字符次数之差。
+#
+# 给你一个字符串 s ，它只包含小写英文字母。请你返回 s 里所有 子字符串的 最大波动 值。
+#
+# 子字符串 是一个字符串的一段连续字符序列。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode.cn/problems/substring-with-largest-variance
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution:
+    def largestVariance(self, s: str) -> int:
+        # TODO 动态规划
+        pass
